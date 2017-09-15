5bce106eb266df3167f6fa33d91dce667f4a69a6
Chris Cardle <cdcardle@gmail.com>
2017-09-15 15:00:14 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i = 1) {
+      [...array, `I am ${i} strange loop.`]
+    } else {
+      [...array, `I am ${i} strange loops.`]
+  }
+} return array
+}
