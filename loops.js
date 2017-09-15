eb507c8d6238ec6612a83656bd443e4abea04a27
Chris Cardle <cdcardle@gmail.com>
2017-09-15 15:23:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,10 +10,10 @@ function forLoop(array) {
 
 function whileLoop(n) {
   while (n > 0) {
-    console.log(`${n}`)
+    console.log(`${n}`);
+    n--;
       while (n = 0) {
         console.log(`done`)
-      n--
       }
   }
 }
